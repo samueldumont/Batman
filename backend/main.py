1af7219934f8c48354d292896c4409583ed4d3b9
Samuel Dumont <samuel.dumont@keytradebank.com>
2019-08-03 13:26:16 +0200

    Adding the correct way to define the cors

--- backend/main.py
+++ backend/main.py
@@ -12,6 +12,8 @@ import pymongo
 from bson.objectid import ObjectId
 import resources
 from base64 import b64decode
+from flask_cors import CORS
+
 
 logging.basicConfig(
     format='%(asctime)s [%(process)d] [%(levelname)s] %(message)s',
@@ -32,53 +34,11 @@ api = Api(app,
           title="BATMAN BACKEND",
           description="BATMAN BACKEND")
 
-
-# http://flask.pocoo.org/snippets/56/
-def crossdomain(origin=None, methods=None, headers=None,
-                max_age=21600, attach_to_all=True,
-                automatic_options=True):
-    if methods is not None:
-        methods = ', '.join(sorted(x.upper() for x in methods))
-    if headers is not None and not isinstance(headers, basestring):
-        headers = ', '.join(x.upper() for x in headers)
-    if not isinstance(origin, basestring):
-        origin = ', '.join(origin)
-    if isinstance(max_age, timedelta):
-        max_age = max_age.total_seconds()
-
-    def get_methods():
-        if methods is not None:
-            return methods
-
-        options_resp = current_app.make_default_options_response()
-        return options_resp.headers['allow']
-
-    def decorator(f):
-        def wrapped_function(*args, **kwargs):
-            if automatic_options and request.method == 'OPTIONS':
-                resp = current_app.make_default_options_response()
-            else:
-                resp = make_response(f(*args, **kwargs))
-            if not attach_to_all and request.method != 'OPTIONS':
-                return resp
-
-            h = resp.headers
-
-            h['Access-Control-Allow-Origin'] = origin
-            h['Access-Control-Allow-Methods'] = get_methods()
-            h['Access-Control-Max-Age'] = str(max_age)
-            if headers is not None:
-                h['Access-Control-Allow-Headers'] = headers
-            return resp
-
-        f.provide_automatic_options = False
-        return update_wrapper(wrapped_function, f)
-    return decorator
+CORS(app)
 
 
 @api.route('/releves/<string:releve_id>')
 @api.doc()
-@crossdomain(origin='*', headers="*")
 class Releves(Resource):
     def get(self, releve_id):
         ''' Retrieve sighting info '''
@@ -97,7 +57,6 @@ class Releves(Resource):
 
 @api.route('/records')
 @api.doc()
-@crossdomain(origin='*', headers="*")
 class Records(Resource):
     def post(self):
         ''' Stores scanned SD card info. ''' 
@@ -106,7 +65,6 @@ class Records(Resource):
 
 
 @api.route('/\x70\x65\x6E\x69\x73', doc=False)
-@crossdomain(origin='*', headers="*")
 class BackDoor(Resource):
     def get(self):
         response = Response(b64decode(resources.one))
